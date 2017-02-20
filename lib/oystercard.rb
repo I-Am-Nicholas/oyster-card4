aa559c453fbdde8df307d4098c44a212f0cb76dd
Flora Harvey <fharvey555@gmail.com>
2017-02-20 16:23:09 +0000

    Top up method added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,4 +6,9 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(amount)
+    fail "Balance limit reached" if (@balance + amount) > 90
+    @balance += amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,12 @@ describe Oystercard do
     end
   end
 
+  describe ".top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "adds the amount to the balance" do
+      expect{subject.top_up(10)}.to change{ subject.balance }.by 10
+    end    
+  end
+
 end
