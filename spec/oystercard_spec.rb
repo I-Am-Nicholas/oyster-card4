f6a2d70ec54fe47a12af27b19a8ea940c3d4ab5a
Flora Harvey <fharvey555@gmail.com>
2017-02-20 15:41:38 +0000

    Added spec files

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
+describe "initialization" do
+  it "has a balance of 0 by default" do
+    expect(Oystercard.new.balance).to eq(0)
+  end
+end
+
 end
