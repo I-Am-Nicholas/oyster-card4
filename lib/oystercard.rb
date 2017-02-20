5ed978161fe0b50f91620136bb903dcb9a265a0a
Flora Harvey <fharvey555@gmail.com>
2017-02-20 16:21:12 +0000

    Add default balance and passed test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,5 +6,4 @@ class Oystercard
     @balance = 0
   end
 
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,10 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
-describe "initialization" do
-  it "has a balance of 0 by default" do
-    expect(Oystercard.new.balance).to eq(0)
+  describe "initialization" do
+    it "has a balance of 0 by default" do
+      expect(subject.balance).to eq(0)
+    end
   end
-end
 
 end
