9ac95262a65d7ef1d3393960e1fba74de2ef962f
Flora Harvey <fharvey555@gmail.com>
2017-02-22 17:59:47 +0000

    Station looks up correct zone when instantiated with name

--- lib/station.rb
+++ lib/station.rb
@@ -2,9 +2,13 @@ require 'zones'
 
 class Station
 
-  # include Zones
-  #
-  # grab_zone
+  include Zones
 
+  attr_reader :zone
+
+  def initialize(name)
+    @name = name
+    @zone = grab_zone(name)
+  end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,18 +4,16 @@ describe Station do
 
   subject(:station) {described_class.new}
 
-  # before :each do
-  #   allow(:station).to receive(:zone).and_return(1)
-  # end
 
-  # describe "initialization" do
-  #   it "gets the correct zone when given a name" do
-  #     expect(station).to eq(1)
-  #   end
-  #
-  # end
+  describe "initialization" do
+    it "gets the correct zone when given a name" do
+      station = Station.new("Euston")
+      expect(station.zone).to eq(1)
+    end
 
-  it { is_expected.to respond_to(grab_zone).with(1).argument}
+  end
+
+
 
 
 
